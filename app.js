f3c64bac009c6f07820c3f523a82933f28da5248
seziimm <sezimnurgazieva34@gmail.com>
2023-01-18 09:54:52 +0300

    test

---
+++ app.js
@@ -0,0 +1,22 @@
+let ArrTag = ['li', 'span', 'div', 'span', 'h3', 'a', 'div', 'span', 'a', 'li', 'div', 'div']
+let result = {}
+ArrTag.forEach(function(a){
+
+    let  jiooii = ArrTag [a]
+    if (result[a] != undefined )
+        ++result[a];
+    else
+        result[a] = 1;
+})
+
+for (var key in result)
+    console.log(ArrTag);
+let tagging = Object.values(result);
+let Arrtag = Object.keys(result);
+
+for (let i = 0; i < tagging.length; i++) {
+    Arrtag[i] = Arrtag[i] + ": " + tagging[i];
+}
+let arrObj = { ...Arrtag };
+console.log(arrObj);
+
